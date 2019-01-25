740bf82a84be8e914105e3dcee63e92483ee93a7
shaj <kolobrod2002@yandex.ru>
2019-01-25 15:08:56 +0300

    Все, кроме разного шага в Shell sort

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <algorithm>
 #include <iterator>
 #include <functional>
+#include <fstream>
 
 #include "oa_sort.h"
 
@@ -27,12 +28,44 @@ struct measure
 
 
 
-int main()
+void read_data(std::istream &is, std::vector<int> &v)
+{
+	std::string slen;
+	std::getline(is, slen);
+	v.clear();
+	v.reserve(std::stoi(slen));
+	for(std::string line; std::getline(is, line);)
+	{
+		v.emplace_back(std::stoi(line));
+	}
+}
+
+void test_vector(const std::vector<int> &v)
+{
+	int cnt = 0;
+	for(auto it = v.begin() + 1; it != v.end(); it++)
+	{
+		if(*it < *(it-1)) cnt++;
+	}
+	if(cnt) std::cout << "array is NOT sorted\n";
+	else std::cout << "array is sorted" << std::endl;
+}
+
+int main(int argc, char const *argv[])
 {
 	{
 		std::vector<int> v {9, 3, 4, 6, 5, 7, 8, 2, 0, 1};
 
 		otusalg::insertion_sort(v);
+
+		std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
+		std::cout << std::endl;
+	}
+
+	{
+		std::vector<int> v {9, 3, 4, 6, 5, 7, 8, 2, 0, 1};
+
+		otusalg::ins_sort(v.begin(), v.end(), std::less<int>());
 
 		std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
 		std::cout << std::endl;
@@ -47,6 +80,63 @@ int main()
 		std::cout << std::endl;
 	}
 
+	// {
+	// 	std::vector<int> v {9, 3, 4, 6, 5, 7, 8, 2, 0, 1};
+
+	// 	otusalg::shell_sort_k(v.begin(), v.end(), std::less<int>());
+
+	// 	std::copy(v.begin(), v.end(), std::ostream_iterator<int>(std::cout, " "));
+	// 	std::cout << std::endl;
+	// }
+
+	if(argc > 1)
+	{
+		std::cout << "'" << argv[1] << "'\n";
+
+		std::vector<int> v;
+		std::ifstream fs;
+ 	    fs.open(argv[1], std::ios::in);
+ 
+	    if(fs.is_open())
+	    {
+	    	read_data(fs, v);
+	    	fs.close();
+	    	test_vector(v);
+	    	std::cout << "data size: " << v.size() << "\n";
+	    	std::cout << "insertion_sort: " << measure<std::chrono::microseconds>::execution([&]()
+	    	{
+	    		otusalg::insertion_sort(v);
+	    	}) << " us\n";
+	    	test_vector(v);
+
+
+	    	fs.open(argv[1]);
+	    	read_data(fs, v);
+	    	fs.close();
+	    	std::cout << "ins_sort: " << measure<std::chrono::microseconds>::execution([&]()
+	    	{
+	    		otusalg::ins_sort(v.begin(), v.end(), std::less<int>());
+	    	}) << " us\n";
+	    	test_vector(v);
+
+
+	    	fs.open(argv[1]);
+	    	read_data(fs, v);
+	    	fs.close();
+	    	std::cout << "shell_sort: " << measure<std::chrono::microseconds>::execution([&]()
+	    	{
+	    		otusalg::shell_sort(v.begin(), v.end(), std::less<int>());
+	    	}) << " us\n";
+	    	test_vector(v);
+
+
+	    }
+	    else
+	    {
+	    	std::cout << "File '" << argv[1] << "' does not exist" << std::endl;
+	    }
+	}
 
 	return 0;
 }
+
--- oa_sort.h
+++ oa_sort.h
@@ -28,14 +28,37 @@ int insertion_sort(std::vector<T> &v)
 	return 0;
 }
 
-template< typename RandomAccessIterator, typename Compare >
-void shell_sort( RandomAccessIterator first, RandomAccessIterator last, Compare comp )
+template <typename RandomAccessIterator, typename Compare>
+void ins_sort(RandomAccessIterator first, RandomAccessIterator last, Compare comp)
 {
-    for( typename std::iterator_traits< RandomAccessIterator >::difference_type d = ( last - first ) / 2; d != 0; d /= 2 )
-        for( RandomAccessIterator i = first + d; i != last; ++i )
-            for( RandomAccessIterator j = i; j - first >= d && comp( *j, *( j - d ) ); j -= d )
-                std::swap( *j, *( j - d ) );
+	for(RandomAccessIterator i = first + 1; i != last; ++i)
+		for(RandomAccessIterator j=i; ((j-first) >= 1) && comp(*j, *(j-1)); --j)
+			std::swap(*j, *(j-1));
 }
 
 
+// Отсюда
+// https://ru.wikipedia.org/wiki/%D0%A1%D0%BE%D1%80%D1%82%D0%B8%D1%80%D0%BE%D0%B2%D0%BA%D0%B0_%D0%A8%D0%B5%D0%BB%D0%BB%D0%B0#%D0%A0%D0%B5%D0%B0%D0%BB%D0%B8%D0%B7%D0%B0%D1%86%D0%B8%D1%8F_%D0%BD%D0%B0_C++
+template< typename RandomAccessIterator, typename Compare >
+void shell_sort( RandomAccessIterator first, RandomAccessIterator last, Compare comp )
+{
+	for( typename std::iterator_traits< RandomAccessIterator >::difference_type d = ( last - first ) / 2; d != 0; d /= 2 )
+		for( RandomAccessIterator i = first + d; i != last; ++i )
+			for( RandomAccessIterator j = i; j - first >= d && comp( *j, *( j - d ) ); j -= d )
+				std::swap( *j, *( j - d ) );
+}
+
+
+template <typename RandomAccessIterator, typename Compare, typename StepIterator>
+void shell_sort_k(RandomAccessIterator first, RandomAccessIterator last, Compare comp, StepIterator steps)
+{
+	for( typename std::iterator_traits< RandomAccessIterator >::difference_type d = ( last - first ) / 2; d != 0; d /= 2 )
+		for( RandomAccessIterator i = first + d; i != last; ++i )
+			for( RandomAccessIterator j = i; j - first >= d && comp( *j, *( j - d ) ); j -= d )
+				std::swap( *j, *( j - d ) );
+}
+
+
+
 } // namespace otusalg
+

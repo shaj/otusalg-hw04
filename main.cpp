9bed22dc58bb2c57e4fd1fdbf77872305612892a
shaj <kolobrod2002@yandex.ru>
2019-01-27 19:14:04 +0300

    Minor changes

--- main.cpp
+++ main.cpp
@@ -49,156 +49,156 @@ int main(int argc, char const *argv[])
 
 		std::vector<int> v;
 		std::ifstream fs;
- 	    fs.open(argv[1], std::ios::in);
+		fs.open(argv[1], std::ios::in);
  
-	    if(fs.is_open())
-	    {
-	    	read_data(fs, v);
-	    	fs.close();
-	    	if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
-	    	else std::cout << "vector NOT sorted\n";
-	    	std::cout << "data size: " << v.size() << "\n";
-	    	std::cout << "insertion_sort: " << measure<std::chrono::microseconds>::execution([&]()
-	    	{
-	    		otusalg::insertion_sort(v);
-	    	}) << " us\n";
-	    	if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
-	    	else std::cout << "vector NOT sorted\n";
-	    	std::cout << std::endl;
+		if(fs.is_open())
+		{
+			read_data(fs, v);
+			fs.close();
+			if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
+			else std::cout << "vector NOT sorted\n";
+			std::cout << "data size: " << v.size() << "\n";
+			std::cout << "insertion_sort: " << measure<std::chrono::microseconds>::execution([&]()
+			{
+				otusalg::insertion_sort(v);
+			}) << " us\n";
+			if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
+			else std::cout << "vector NOT sorted\n";
+			std::cout << std::endl;
 
 
-	    	fs.open(argv[1]);
-	    	read_data(fs, v);
-	    	fs.close();
-	    	std::cout << "shell_sort: " << measure<std::chrono::microseconds>::execution([&]()
-	    	{
-	    		otusalg::shell_sort(v.begin(), v.end(), std::less<int>());
-	    	}) << " us\n";
-	    	if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
-	    	else std::cout << "vector NOT sorted\n";
-	    	std::cout << std::endl;
+			fs.open(argv[1]);
+			read_data(fs, v);
+			fs.close();
+			std::cout << "shell_sort: " << measure<std::chrono::microseconds>::execution([&]()
+			{
+				otusalg::shell_sort(v.begin(), v.end(), std::less<int>());
+			}) << " us\n";
+			if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
+			else std::cout << "vector NOT sorted\n";
+			std::cout << std::endl;
 
 
-	    	fs.open(argv[1]);
-	    	read_data(fs, v);
-	    	fs.close();
-	    	std::cout << "shell_sort_c: " << measure<std::chrono::microseconds>::execution([&]()
-	    	{
-	    		otusalg::shell_sort_c(v);
-	    	}) << " us\n";
-	    	if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
-	    	else std::cout << "vector NOT sorted\n";
-	    	std::cout << std::endl;
+			fs.open(argv[1]);
+			read_data(fs, v);
+			fs.close();
+			std::cout << "shell_sort_c: " << measure<std::chrono::microseconds>::execution([&]()
+			{
+				otusalg::shell_sort_c(v);
+			}) << " us\n";
+			if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
+			else std::cout << "vector NOT sorted\n";
+			std::cout << std::endl;
 
 
-	    	fs.open(argv[1]);
-	    	read_data(fs, v);
-	    	fs.close();
-	    	std::cout << "std::sort: " << measure<std::chrono::microseconds>::execution([&]()
-	    	{
-	    		std::sort(v.begin(), v.end(), std::less<int>());
-	    	}) << " us\n";
-	    	if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
-	    	else std::cout << "vector NOT sorted\n";
-	    	std::cout << std::endl;
+			fs.open(argv[1]);
+			read_data(fs, v);
+			fs.close();
+			std::cout << "std::sort: " << measure<std::chrono::microseconds>::execution([&]()
+			{
+				std::sort(v.begin(), v.end(), std::less<int>());
+			}) << " us\n";
+			if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
+			else std::cout << "vector NOT sorted\n";
+			std::cout << std::endl;
 
 
 
-	    	std::cout << "\nDifferent sequence of steps:\n";
-	    	std::vector<int> d;
+			std::cout << "\nDifferent sequence of steps:\n";
+			std::vector<int> d;
 
-	    	fs.open(argv[1]);
-	    	read_data(fs, v);
-	    	fs.close();
-	    	for(int i=v.size()/2; i!=0; i /= 2) d.emplace_back(i);
+			fs.open(argv[1]);
+			read_data(fs, v);
+			fs.close();
+			for(int i=v.size()/2; i!=0; i /= 2) d.emplace_back(i);
 			std::copy(d.begin(), d.end(), std::ostream_iterator<int>(std::cout, " "));
 			std::cout << "\n";
 
-	    	std::cout << "shell_sort_ck steps d[1]=N/2; d[i]=d[i-1]/2: " << measure<std::chrono::microseconds>::execution([&]()
-	    	{
-	    		otusalg::shell_sort_ck(v, d);
-	    	}) << " us\n";
-	    	if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
-	    	else std::cout << "vector NOT sorted\n";
-	    	std::cout << std::endl;
+			std::cout << "shell_sort_ck steps d[1]=N/2; d[i]=d[i-1]/2: " << measure<std::chrono::microseconds>::execution([&]()
+			{
+				otusalg::shell_sort_ck(v, d);
+			}) << " us\n";
+			if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
+			else std::cout << "vector NOT sorted\n";
+			std::cout << std::endl;
 
 
-	    	fs.open(argv[1]);
-	    	read_data(fs, v);
-	    	fs.close();
-	    	d.clear();
-	    	for(int i=1; ; i++)
-    		{
-    			int p = std::pow(2, i) - 1;
-    			if(p > v.size()) break;
-    			d.emplace_back(p);
-    		}
-    		std::reverse(d.begin(), d.end());
+			fs.open(argv[1]);
+			read_data(fs, v);
+			fs.close();
+			d.clear();
+			for(int i=1; ; i++)
+			{
+				int p = std::pow(2, i) - 1;
+				if(p > v.size()) break;
+				d.emplace_back(p);
+			}
+			std::reverse(d.begin(), d.end());
 			std::copy(d.begin(), d.end(), std::ostream_iterator<int>(std::cout, " "));
 			std::cout << "\n";
 
-	    	std::cout << "shell_sort_ck steps 2**i-1: " << measure<std::chrono::microseconds>::execution([&]()
-	    	{
-	    		otusalg::shell_sort_ck(v, d);
-	    	}) << " us\n";
-	    	if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
-	    	else std::cout << "vector NOT sorted\n";
-	    	std::cout << std::endl;
+			std::cout << "shell_sort_ck steps 2**i-1: " << measure<std::chrono::microseconds>::execution([&]()
+			{
+				otusalg::shell_sort_ck(v, d);
+			}) << " us\n";
+			if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
+			else std::cout << "vector NOT sorted\n";
+			std::cout << std::endl;
 
-	    	
-	    	fs.open(argv[1]);
-	    	read_data(fs, v);
-	    	fs.close();
-	    	d.clear();
-	    	for(int i=0; ; i++)
-    		{
-    			int p = std::pow(2, i);
-    			if((p > v.size())) break;
-    			d.emplace_back(p);
-    		}
-    		std::reverse(d.begin(), d.end());
+			
+			fs.open(argv[1]);
+			read_data(fs, v);
+			fs.close();
+			d.clear();
+			for(int i=0; ; i++)
+			{
+				int p = std::pow(2, i);
+				if((p > v.size())) break;
+				d.emplace_back(p);
+			}
+			std::reverse(d.begin(), d.end());
 			std::copy(d.begin(), d.end(), std::ostream_iterator<int>(std::cout, " "));
 			std::cout << "\n";
 
-	    	std::cout << "shell_sort_ck steps 2**i: " << measure<std::chrono::microseconds>::execution([&]()
-	    	{
-	    		otusalg::shell_sort_ck(v, d);
-	    	}) << " us\n";
-	    	if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
-	    	else std::cout << "vector NOT sorted\n";
-	    	std::cout << std::endl;
+			std::cout << "shell_sort_ck steps 2**i: " << measure<std::chrono::microseconds>::execution([&]()
+			{
+				otusalg::shell_sort_ck(v, d);
+			}) << " us\n";
+			if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
+			else std::cout << "vector NOT sorted\n";
+			std::cout << std::endl;
 
 
-	    	fs.open(argv[1]);
-	    	read_data(fs, v);
-	    	fs.close();
-	    	d.clear();
-	    	d.push_back(1750);
-	    	d.push_back(701);
-	    	d.push_back(301);
-	    	d.push_back(132);
-	    	d.push_back(57);
-	    	d.push_back(23);
-	    	d.push_back(10);
-	    	d.push_back(4);
-	    	d.push_back(1);
+			fs.open(argv[1]);
+			read_data(fs, v);
+			fs.close();
+			d.clear();
+			d.push_back(1750);
+			d.push_back(701);
+			d.push_back(301);
+			d.push_back(132);
+			d.push_back(57);
+			d.push_back(23);
+			d.push_back(10);
+			d.push_back(4);
+			d.push_back(1);
 			std::copy(d.begin(), d.end(), std::ostream_iterator<int>(std::cout, " "));
 			std::cout << "\n";
 
-	    	std::cout << "shell_sort_ck steps Marcin Ciura: " << measure<std::chrono::microseconds>::execution([&]()
-	    	{
-	    		otusalg::shell_sort_ck(v, d);
-	    	}) << " us\n";
-	    	if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
-	    	else std::cout << "vector NOT sorted\n";
-	    	std::cout << std::endl;
+			std::cout << "shell_sort_ck steps Marcin Ciura: " << measure<std::chrono::microseconds>::execution([&]()
+			{
+				otusalg::shell_sort_ck(v, d);
+			}) << " us\n";
+			if(std::is_sorted(v.begin(), v.end())) std::cout << "vector sorted\n";
+			else std::cout << "vector NOT sorted\n";
+			std::cout << std::endl;
 
 
-	    }
-	    else
-	    {
-	    	std::cout << "File '" << argv[1] << "' does not exist" << std::endl;
-	    }
+		}
+		else
+		{
+			std::cout << "File '" << argv[1] << "' does not exist" << std::endl;
+		}
 	}
 
 	return 0;
